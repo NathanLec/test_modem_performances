bfad1575f2b93a822e794dea0e6729b491c9407d
NathanLec <lecluse.eleve@ecole-navale.fr>
2021-10-27 11:23:24 +0200

    v5
    Fonctionnal test

--- test_performances_instant_message.py
+++ test_performances_instant_message.py
@@ -1,43 +1,50 @@
 import subprocess
+import time
 
 # Reception of the text with the modems
+while True:
+    HOST = "192.168.0.189"
+    PORT = "9200"
 
-HOST = "192.168.0.189"
-PORT = "9200"
+    name_receive_file = 'file_receive.txt'
 
-name_receive_file = 'file_receive.txt'
+    subprocess.run('echo "" | cat > %s' %(name_receive_file),shell=True)
 
-subprocess.run("nc -W 2 %s %s > %s" % (HOST,PORT,name_receive_file),shell=True)
+    GO = False
+    LEN = 0
 
+
+    subprocess.run("nc -W 1 %s %s >> %s" %  (HOST,PORT,name_receive_file),shell = True)
+
+    while not(GO):
+        a = str(subprocess.check_output("cat %s" %(name_receive_file),shell=True))
+        if len(a) == LEN:
+            GO = True
+        else:
+            LEN = len(a)
+            subprocess.run("nc -w 1 %s %s >> %s" % (HOST,PORT,name_receive_file),shell=True)
 
 # Send file with ssh
 
-command = open("%s"%(name_receive_file),"r+")
-txt = command.readlines(1)
-command.close()
+    command = open("%s"%(name_receive_file),"r+")
+    txt = command.read()
+    command.close()
 
-print(txt)
+    txt = txt.replace("\n","")
 
-txt = str(txt)
+    user = 'ubuntu'
+    host = '192.168.140.253'
+    pw = 'nathan1003'
 
-txt = str(txt[:-4])
+    file = "dev_ws/src/file1.txt"
 
-txt = txt.split(',')[-1]
+    command = 'echo %s | cat > %s' % (txt,file)
 
-print('texte= ', txt)
+    subprocess.run("sshpass -p %s ssh %s@%s '%s'" % (pw,user,host,command), shell=True)
 
-user = 'ubuntu'
-host = '192.168.78.253'
-pw = 'nathan1003'
+    time.sleep(2)
 
-file = "dev_ws/src/file1.txt"
+ # Send confirmation for the sent
 
-command = 'echo %s | cat > %s' % (txt,file)
-
-subprocess.run("sshpass -p %s ssh %s@%s '%s'" % (pw,user,host,command), shell=True)
-
-# Send confirmation for the sent
-
-subprocess.run("echo 'AT*SENDIM,2,255,noack,OK' | nc -N %s %s" % (HOST,PORT),shell=True)
-subprocess.run("nc -W 1 %s %s" % (HOST,PORT),shell=True)
+    subprocess.run("echo 'OK' | nc -N %s %s" % (HOST,PORT),shell=True)
 
--- test_performances_instant_message_rasp.py
+++ test_performances_instant_message_rasp.py
@@ -1,3 +1,4 @@
+
 import subprocess
 import random as rd
 import time
@@ -30,25 +31,21 @@ def send_message(leng):
     HOST = "192.168.0.198"
     PORT = "9200"
 
+
     txtlen = str(len(txt))
-    subprocess.run("echo 'AT*SENDIM,%s,255,noack,%s' | nc -N %s %s" % (txtlen,txt,HOST,PORT),shell=True)
+
+
+    subprocess.run("echo '%s' | nc -W 1 %s %s" % (txt,HOST,PORT),shell=True)
 
 
     ## Wait for the acknoledge
 
-    WAIT = True
-
         ## Importation of the texts
-    while WAIT:
-        receive = open("%s"%(name_receive_file),"r+")
-        text_receive = receive.read()
-        if len(text_receive)!=0 :
-            WAIT = False
-            receive.close()
-            subprocess.run("cat '' > %s" %(name_receive_file),shell=True)
+    receive = open("%s"%(name_receive_file),"r+")
+    text_receive = receive.read()
+    receive.close()
+    subprocess.run("echo "" | cat > %s" %(name_receive_file),shell=True)
     RESULT = comparison(txt, text_receive)
-    print(txt)
-    print(text_receive)
     return RESULT
 
 
@@ -56,18 +53,17 @@ def send_message(leng):
     ## Code of comparison
 
 def comparison(text_send,text_receive):
-    tic = time.time()
     length = 0
     diff = 0
     gap = 0
-    
+
     long1 = len(text_send)
-    long2 = len(text_receive)
-    
+    long2 = len(text_receive)-1
+
     long = long1
     text_l = text_send
     text_s = text_receive
-    
+
     if long1 != long2:
         length = long1-long2
         if long1 < long2:
@@ -75,8 +71,8 @@ def comparison(text_send,text_receive):
             text_s = text_send
             text_l = text_receive
         else:
-            long = long2    
-            
+            long = long2
+
     for k in range(long):
         if k+gap < max(long1,long2):
             if text_s[k] != text_l[k+gap]:
@@ -87,8 +83,6 @@ def comparison(text_send,text_receive):
                         diff += 1
                 else:
                     diff += 1
-    tac = time.time()       
-    print("Comparison : ",tac-tic)
     return (length,diff,gap)
 
 
@@ -101,26 +95,14 @@ def comparison(text_send,text_receive):
 
 
 
+Value = [10, 20, 50, 100, 200, 500, 1000, 2000]
 
+RESULT = []
+k = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for long in Value:
+    tic = time.time()
+    (r1,r2,r3) = send_message(long)
+    t = time.time()-tic
+    RESULT.append((t,r1,r2,r3))
+print(RESULT)

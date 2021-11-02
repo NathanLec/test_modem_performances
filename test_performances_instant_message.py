1c820fa4e85dd8294582efb58960d5d84da01133
NathanLec <lecluse.eleve@ecole-navale.fr>
2021-11-02 11:11:01 +0100

    Add of commentaries

--- test_performances_instant_message.py
+++ test_performances_instant_message.py
@@ -1,50 +1,73 @@
+### Communication test with the modems ###
+### Using the burst messages of the Evologics Modems ###
+
+### This algorithm, in the computer, wait for a send from the Raspberry throught the modems and send back the result, in order to compare. ###
+
+### Press control C when the modems stop to transmit (end of the noise of the transmission) and when the algorith told you to. ###
+
 import subprocess
 import time
 
-# Reception of the text with the modems
+## Reception of the text with the modems
 while True:
+    # IP address of the modem
     HOST = "192.168.0.189"
     PORT = "9200"
-
+    
+    # File where the receive message is writen 
     name_receive_file = 'file_receive.txt'
 
+    # Write above the file, make it empty for the others messages
     subprocess.run('echo "" | cat > %s' %(name_receive_file),shell=True)
 
-    GO = False
-    LEN = 0
-
-
+    # Initialization of the receive loop
+    GO = False   # Do you have to leave the loop or not
+    LEN = 0      # Condition of length of the message
+    pr = 1       # Question for the printing part
+    
+    # Wait for the first message which arrive ( -W 1 ) to enter the loop and add it in the file
     subprocess.run("nc -W 1 %s %s >> %s" %  (HOST,PORT,name_receive_file),shell = True)
+    
+    tic = time.time()
+    
+    # Receive loop
+    try:                                                                                                # Allow to get out of the loop whenever the user wants with the ^C command
+        while not(GO):
+            a = str(subprocess.check_output("cat %s" %(name_receive_file),shell=True))                  # Read the current text in the received file
+            if len(a) == LEN                                                                            # Compare the length of the current text with the old one and if you have to write a message
+                if pr == 1:                                                                             # As the consequence, if the length is the same and if you never wrote, the algorith told you to stop
+                    print("^C to stop")                                                                 # Because, front its point of vue, it didn't receive a message the last second, so it's finish
+                    pr = 0                                                                              # IF THE MODEMS STILL TRANSMIT, DO NOT STOP, it will make the futur receptions.
+            else:
+                LEN = len(a)                                                                            # If the length change, it's because the algorith receive additionnal data.
+                pr = 1                                                                                  # As the consequence it will have to write when it will be finish for it
+            subprocess.run("nc -w 1 %s %s >> %s" % (HOST,PORT,name_receive_file),shell=True)            # And you listen for one more second, trying to get more informations
+    except KeyboardInterrupt:                                       # This is for capture the ^C command and get of the while loop
+        GO = True
+#        print(a)                                                    # It can print the output of the text file, if you want to compare yourself
+#    print("time receive = ",time.time()-tic)                        # This line print the time that the modems had to receive the all file. It is only informative, because it require your stop action
 
-    while not(GO):
-        a = str(subprocess.check_output("cat %s" %(name_receive_file),shell=True))
-        if len(a) == LEN:
-            GO = True
-        else:
-            LEN = len(a)
-            subprocess.run("nc -w 1 %s %s >> %s" % (HOST,PORT,name_receive_file),shell=True)
+## Send file with ssh
 
-# Send file with ssh
-
-    command = open("%s"%(name_receive_file),"r+")
-    txt = command.read()
+    command = open("%s"%(name_receive_file),"r+")                                               # Open the receive text file
+    txt = command.read()                                                                        # Read the all text
     command.close()
 
-    txt = txt.replace("\n","")
+    txt = txt.replace("\n","")                                                                  # Change the text to make it similar to the send one (remove the caracters created by python and the subprocess library)
 
-    user = 'ubuntu'
-    host = '192.168.140.253'
+    user = 'ubuntu'                                                                             # IP address and password for the Raspberry Pi SSH connexion
+    host = '192.168.45.253'
     pw = 'nathan1003'
 
-    file = "dev_ws/src/file1.txt"
+    file = "dev_ws/src/file1.txt"                                                               # Name of the file in the Raspberry Pi, take care to write the same in the Raspberry pi algorithm
 
-    command = 'echo %s | cat > %s' % (txt,file)
+    command = 'echo %s | cat > %s' % (txt,file)                                                 # Command to write the text in this file
 
-    subprocess.run("sshpass -p %s ssh %s@%s '%s'" % (pw,user,host,command), shell=True)
+    subprocess.run("sshpass -p %s ssh %s@%s '%s'" % (pw,user,host,command), shell=True)         # Use of sshpass to automate the write
 
-    time.sleep(2)
+    time.sleep(2)                                                                               # Time wait for the command to finish in the raspberry
 
- # Send confirmation for the sent
+ ## Send confirmation for the sent through the modem
 
-    subprocess.run("echo 'OK' | nc -N %s %s" % (HOST,PORT),shell=True)
+    subprocess.run("echo 'OK' | nc -N %s %s" % (HOST,PORT),shell=True)                          # When all these steps are finished, sent of the confirmation throught the Modems
 
--- test_performances_instant_message_rasp.py
+++ test_performances_instant_message_rasp.py
@@ -1,61 +1,59 @@
+### Communication test with the modems ###
+### Using the burst messages of the Evologics Modems ###
+
+### This algorithm, in the Raspberry Pi 4, send a message through the modems and wait for a send from the Computer throught the SSH compare the result ###
+
 
 import subprocess
 import random as rd
 import time
 
 
-def random_message(leng):
-    tic = time.time()
+def random_message(leng):                   # Random message creation with the length
+    tic = time.time()                       # Initialization of the time counter for the creation of the message
     txt = ''
     for k in range(leng):
-        lettre = rd.randint(65,90)
+        lettre = rd.randint(65,90)          # Random capital letter with the ASCII code
         txt = txt + chr(lettre)
-    tac = time.time()
+    tac = time.time()                       #Time of the creation of the message
     print("message : ",tac-tic)
     return txt
 
-def send_message(leng):
+def send_message(leng):                     # Send the message of a determined length and wait for the Computer acknoledge
     ## Variables
 
-    name_receive_file = 'file1.txt'
-
+    name_receive_file = 'file1.txt'         # Creation of the file where you will receive the acknoledge message from the Raspberry (THINK TO CHANGE IN THE COMPUTER ALGORITHM)
 
     ## Creation of the random text
 
-    txt = random_message(leng)
-
+    txt = random_message(leng)              # Creation of the random message
     print(txt)
 
     ## Send file with modems
 
-    HOST = "192.168.0.198"
+    HOST = "192.168.0.198"                  # IP address of the modem
     PORT = "9200"
 
-
-    txtlen = str(len(txt))
-
-
-    subprocess.run("echo '%s' | nc -W 1 %s %s" % (txt,HOST,PORT),shell=True)
+    subprocess.run("echo '%s' | nc -W 1 %s %s" % (txt,HOST,PORT),shell=True)    # Send the message as a Burst message through the modems and wait for a 1 ligne answer ( -W 1 ) which be the OK answer
 
 
     ## Wait for the acknoledge
 
-        ## Importation of the texts
-    receive = open("%s"%(name_receive_file),"r+")
+    ## Importation of the texts
+    receive = open("%s"%(name_receive_file),"r+")                               # Read and write over the old receive file, and wait for the next text.
     text_receive = receive.read()
     receive.close()
-    subprocess.run("echo "" | cat > %s" %(name_receive_file),shell=True)
+    subprocess.run("echo "" | cat > %s" %(name_receive_file),shell=True)        # Write over the file
     RESULT = comparison(txt, text_receive)
     return RESULT
 
 
-
     ## Code of comparison
 
-def comparison(text_send,text_receive):
-    length = 0
-    diff = 0
-    gap = 0
+def comparison(text_send,text_receive):                     # Compare the 2 texts with 3 parameters
+    length = 0                                              # Difference of the length of the two files
+    diff = 0                                                # Difference of the caracters
+    gap = 0                                                 # If their is a removed caracter, and all the rest is identical, the gap will increase of 1 and their will be no diff
 
     long1 = len(text_send)
     long2 = len(text_receive)-1
@@ -64,26 +62,27 @@ def comparison(text_send,text_receive):
     text_l = text_send
     text_s = text_receive
 
-    if long1 != long2:
-        length = long1-long2
-        if long1 < long2:
+    if long1 != long2:                                      # If the length is different
+        length = long1-long2                                # Put the value of the difference between the 2 texts
+        if long1 < long2:                                   # Choose the shorter text, to see if their is a additionnal caracter or a remove caracter
             long = long1
             text_s = text_send
             text_l = text_receive
         else:
             long = long2
 
-    for k in range(long):
-        if k+gap < max(long1,long2):
-            if text_s[k] != text_l[k+gap]:
-                if k+gap+1 < max(long1,long2):
-                    if text_l[k+gap+1] == text_s[k]:
-                        gap += 1
+    for k in range(long):                                   # In the range of the shorter message
+        if k+gap < max(long1,long2):                        # Try if the gap is not out of range
+            if text_s[k] != text_l[k+gap]:                  # If the two caracters are differents :
+                if k+gap+1 < max(long1,long2):                  # If the next caracter is in range, and 
+                    if text_l[k+gap+1] == text_s[k]:                # If the next caracter is identical with the previous caracter,
+                        gap += 1                                    # It should be a gap
                     else:
-                        diff += 1
+                        diff += 1                               # else it just a difference
                 else:
                     diff += 1
-    return (length,diff,gap)
+    return (length,diff,gap)                                # Result of the comparison
+
 
 
 # M = random_message(10**7)
@@ -95,7 +94,7 @@ def comparison(text_send,text_receive):
 
 
 
-Value = [10, 20, 50, 100, 200, 500, 1000, 2000]
+Value = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]    # Length of the different messages to send
 
 RESULT = []
 k = 0
@@ -104,5 +103,10 @@ for long in Value:
     tic = time.time()
     (r1,r2,r3) = send_message(long)
     t = time.time()-tic
-    RESULT.append((t,r1,r2,r3))
+    RESULT.append((long,t,r1,r2,r3))
+    print(RESULT[-1])
+print("""
+
+
+""")
 print(RESULT)
